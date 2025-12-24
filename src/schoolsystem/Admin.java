bb5581684368e35ed14f4ded99b98d5af82c9309
NoorYounes14 <nooryounes14@gmail.com>
2025-12-24 22:07:03 +0200

    Add full code for User and Admin classes

--- src/schoolsystem/Admin.java
+++ src/schoolsystem/Admin.java
@@ -1,13 +1,110 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package schoolsystem;
 
-/**
- *
- * @author Lenovo
- */
-public class Admin {
-    
+import java.util.ArrayList;
+
+public class Admin extends User {
+
+    private ArrayList<Student> students;
+
+    private ArrayList<Professor> professors;
+
+    public Admin(int id, String name, String email, String password) {
+
+        super(id, name, email, password);
+
+        students = new ArrayList<>();
+
+        professors = new ArrayList<>();
+
+    }
+
+    public void addNewStudent(Student student) {
+
+        students.add(student);
+
+        System.out.println("Student added: " + student.getName());
+
+    }
+
+    public void editStudent(Student student, String newname, String newEmail) {
+
+        student.setName(newname);
+
+        student.setEmail(newEmail);
+
+        System.out.println("Student updated: " + student.getName());
+
+    }
+
+    public void addNewTeacher(Professor professor) {
+
+        professors.add(professor);
+
+        System.out.println("Teacher added: " + professor.getName());
+
+    }
+
+    public void viewAllTimetables() {
+
+        System.out.println("Students Timetables:");
+
+        for (Student s : students) {
+
+            System.out.println(s.getName());
+
+            s.viewTimetable();
+
+        }
+
+        System.out.println("Professors Timetables:");
+
+        for (Professor p : professors) {
+
+            System.out.println(p.getName());
+
+            p.viewTimetable();
+
+        }
+
+    }
+
+    public void viewTeacherTimetables() {
+
+        for (Professor p : professors) {
+
+            System.out.println(p.getName());
+
+            p.viewTimetable();
+
+        }
+
+    }
+
+    public ArrayList<Student> getStudents() {
+
+        return students;
+
+    }
+
+    public ArrayList<Professor> getProfessors() {
+
+        return professors;
+
+    }
+
+    @Override
+
+    public Admin login(String email, String password) {
+
+        if (getEmail().equals(email) && getPassword().equals(password)) {
+
+            return this;
+
+        }
+
+        return null;
+
+    }
+
 }
+
--- src/schoolsystem/User.java
+++ src/schoolsystem/User.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package schoolsystem;
 
-/**
- *
- * @author Lenovo
- */
-public class User {
-    
+public abstract class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract User login(String email, String password);
 }
